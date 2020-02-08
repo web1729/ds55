584563879d49115ff7658d88586ce27ddb9c2f80
web1729 <47570965+web1729@users.noreply.github.com>
2020-02-08 15:19:49 +0530

    yesss

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    Tabletop.init({
+      key: 'https://docs.google.com/spreadsheets/d/1JLB4jGeuGZp77X7uF_GCHeApazGM4kUMj-VUh2gilRw/edit?usp=sharing',
+      callback: function(data, tabletop) {
+        var i,
+            dataLength = data.length;
+  
+        for (i=0; i<dataLength; i++) {
+          $('#politicians').append(
+            $('<li>', {
+              text: data[i].Name + ', ' + data[i].Name
+            })
+          );
+        }
+      },
+      simpleSheet: true
+    });
+  });
