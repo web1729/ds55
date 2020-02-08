87e82006adaacba26099fd19f6b7d2375582f341
web1729 <47570965+web1729@users.noreply.github.com>
2020-02-08 20:29:08 +0530

    erererer

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 $(document).ready(function () {
-alert("working");
+    alert("working");
     Tabletop.init({
       key: 'https://docs.google.com/spreadsheets/d/1JLB4jGeuGZp77X7uF_GCHeApazGM4kUMj-VUh2gilRw/edit?usp=sharing',
       callback: function(data, tabletop) {
-        var i,
-            dataLength = data.length;
+        var i,dataLength = data.length;
   
         for (i=0; i<dataLength; i++) {
           $('#politicians').append(
             $('<li>', {
-              text: data[i].Name + ', ' + data[i].Name
+              text: data[i].Name + ', ' + data[i].Name 
             })
           );
         }
       },
       simpleSheet: true
     });
-window.addEventListener('DOMContentLoaded', init)
+//window.addEventListener('DOMContentLoaded', init);
   });
